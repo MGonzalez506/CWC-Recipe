8ad69e4b1deb088c041e27b6e63e9f1ec8c5c4bb
mgonzalez506 <MGonzalez506@users.noreply.github.com>
2023-01-14 14:28:26 -0600

    Stable check if combination exists
    Check if combination of card number and suit already exists in the array.
    This is done through card Structure Equatable, which is a special protocol that allow the code to perform compilations, sorting, and others.

--- CWC-Receta-Challenges/CWC-Receta-Challenges/ContentView.swift
+++ CWC-Receta-Challenges/CWC-Receta-Challenges/ContentView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct card {
+struct card:Equatable {
     var cardNumber = 1
     var cardSuit = "Spades"
 }
@@ -15,7 +15,7 @@ struct card {
 struct ContentView: View {
     
     // Create an instance of your playing card structure
-    var deck:[card] = [card]()
+    @State var deck:[card] = [card]()
     // Create an array with the 4 main suits
     var suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
     // Create an array with the 4 special Cards in a deck
@@ -38,6 +38,17 @@ struct ContentView: View {
                     newCard.cardNumber = Int.random(in: 1...13)
                     // Assign to newCard a random suit from the 4 available
                     newCard.cardSuit = suits[Int.random(in: 0...3)]
+                    // Check if combination is already in array
+                    if deck.contains(newCard){
+                        // The combination is not in the array
+                        // Therefore append it
+                        print("Ya existía: \(newCard.cardNumber) suit: \(newCard.cardSuit)")
+                    } else {
+                        // The combination is alreay in the array
+                        // Therefore do not append
+                        print("New combination: \(newCard.cardNumber) suit: \(newCard.cardSuit)")
+                        deck.append(newCard)
+                    }
                 }
                 Spacer()
                 Spacer()
