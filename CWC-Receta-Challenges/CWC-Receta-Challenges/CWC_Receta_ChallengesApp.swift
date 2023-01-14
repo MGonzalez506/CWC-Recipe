89c7daeffa01afb2b2a2bce471459c6ae2931080
mgonzalez506 <MGonzalez506@users.noreply.github.com>
2023-01-14 10:27:01 -0600

    iOS App Challenges Barebones
    Initial code to start working on specific challenges a long the way.

---
+++ CWC-Receta-Challenges/CWC-Receta-Challenges/CWC_Receta_ChallengesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWC_Receta_ChallengesApp.swift
+//  CWC-Receta-Challenges
+//
+//  Created by MIGUEL GONZALEZ on 14/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CWC_Receta_ChallengesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
