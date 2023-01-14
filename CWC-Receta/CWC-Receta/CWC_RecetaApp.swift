493046a50c2bf6e7a0a898b595984bbb3d69f1da
mgonzalez506 <MGonzalez506@users.noreply.github.com>
2023-01-14 09:35:15 -0600

    Barebones, Initial Code

---
+++ CWC-Receta/CWC-Receta/CWC_RecetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWC_RecetaApp.swift
+//  CWC-Receta
+//
+//  Created by MIGUEL GONZALEZ on 14/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CWC_RecetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
