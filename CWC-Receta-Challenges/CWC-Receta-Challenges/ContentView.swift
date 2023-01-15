da845650f0edd60717f55b8b36857c11235676e6
mgonzalez506 <MGonzalez506@users.noreply.github.com>
2023-01-15 12:20:09 -0600

    Add ListView to the screen

--- CWC-Receta-Challenges/CWC-Receta-Challenges/ContentView.swift
+++ CWC-Receta-Challenges/CWC-Receta-Challenges/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Spacer()
+            List(numbers, id: \.self) { numberElement in
+                Text(String(numberElement))
+            }
             Spacer()
             HStack {
                 Button("ADD DIGIT") {
