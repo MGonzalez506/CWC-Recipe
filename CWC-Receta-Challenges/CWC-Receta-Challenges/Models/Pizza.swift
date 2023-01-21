303be748a858ece1b0db5d38b181f168d30fd804
mgonzalez506 <MGonzalez506@users.noreply.github.com>
2023-01-21 14:18:32 -0600

    Create class Pizza in Models folder

---
+++ CWC-Receta-Challenges/CWC-Receta-Challenges/Models/Pizza.swift
@@ -0,0 +1,15 @@
+//
+//  Pizza.swift
+//  CWC-Receta-Challenges
+//
+//  Created by MIGUEL GONZALEZ on 21/1/23.
+//
+
+import Foundation
+
+class Pizza {
+    var name = ""
+    var topping1 = ""
+    var topping2 = ""
+    var topping3 = ""
+}
