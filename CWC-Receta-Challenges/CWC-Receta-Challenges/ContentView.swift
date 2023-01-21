26ded7a0015f9c26451ac650da14fc664568fc32
mgonzalez506 <MGonzalez506@users.noreply.github.com>
2023-01-21 10:18:55 -0600

    Add an array of 4 possible names

--- CWC-Receta-Challenges/CWC-Receta-Challenges/ContentView.swift
+++ CWC-Receta-Challenges/CWC-Receta-Challenges/ContentView.swift
@@ -18,6 +18,9 @@ class TalkShowHosts {
 }
 
 struct ContentView: View {
+    // Declare string that contains 4 names
+    let names = ["Miguel", "Silvia", "Carmen", "Karely"]
+    
     var body: some View {
         VStack {
             Button("Generate Struct Host Name") {
@@ -30,6 +33,7 @@ struct ContentView: View {
         var host = TalkShowHost()
         
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
