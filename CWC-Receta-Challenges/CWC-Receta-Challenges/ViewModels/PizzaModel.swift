0fbad94d1fdd47c5819faee497e1636d510fa41b
mgonzalez506 <MGonzalez506@users.noreply.github.com>
2023-01-21 14:19:28 -0600

    Create a list of Pizza model

---
+++ CWC-Receta-Challenges/CWC-Receta-Challenges/ViewModels/PizzaModel.swift
@@ -0,0 +1,13 @@
+//
+//  PizzaModel.swift
+//  CWC-Receta-Challenges
+//
+//  Created by MIGUEL GONZALEZ on 21/1/23.
+//
+
+import Foundation
+
+class PizzaModel {
+    // Create a list of Pizza model
+    var pizzas = [Pizza]()
+}
