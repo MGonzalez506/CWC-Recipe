2e07576295357fdc10fcf23316e199089795b0aa
mgonzalez506 <MGonzalez506@users.noreply.github.com>
2023-01-21 13:53:10 -0600

    Create an empty list of recipes

---
+++ CWC-Receta/CWC-Receta/ViewModels/RecipeModel.swift
@@ -0,0 +1,13 @@
+//
+//  RecipeModel.swift
+//  CWC-Receta
+//
+//  Created by MIGUEL GONZALEZ on 21/1/23.
+//
+
+import Foundation
+
+class RecipeModel {
+    // Create an empty list of recipes
+    var recipes = [Recipe]()
+}
