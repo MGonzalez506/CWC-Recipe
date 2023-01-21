d99076e4cf4a1a2b0681870941f5addfba897479
mgonzalez506 <MGonzalez506@users.noreply.github.com>
2023-01-21 09:43:05 -0600

    Move all data to ViewTypeOfPerson
    Move all contents from L5 to a new view called TypeOfPerson

--- CWC-Receta-Challenges/CWC-Receta-Challenges/CWC_Receta_ChallengesApp.swift
+++ CWC-Receta-Challenges/CWC-Receta-Challenges/CWC_Receta_ChallengesApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CWC_Receta_ChallengesApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ViewTypeOfPerson()
         }
     }
 }
