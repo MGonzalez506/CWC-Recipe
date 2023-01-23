905a1388c10357ac3d76906c8461fe18a88b82d2
mgonzalez506 <MGonzalez506@users.noreply.github.com>
2023-01-23 14:32:50 -0600

    Move all challenge L8 to a different view
    Move all challenge L8 to a different view, called ViewPizzas

--- CWC-Receta-Challenges/CWC-Receta-Challenges/Views/ContentView.swift
+++ CWC-Receta-Challenges/CWC-Receta-Challenges/Views/ContentView.swift
@@ -8,27 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    // Create instance of PizzaModel
-    @ObservedObject var pizzas = PizzaModel()
     
     var body: some View {
-        VStack {
-            List(pizzas.pizzasList) { pizza in
-                Text(pizza.name)
-                    .font(.title)
-                HStack {
-                    Text(pizza.topping1)
-                    Text("-")
-                    Text(pizza.topping2)
-                    Text("-")
-                    Text(pizza.topping3)
-                }
-            }
-            Button("Add Pinapple") {
-                pizzas.addPinapple()
-            }
-            .buttonStyle(BasicBtnStyle())
-        }
+        Text("Hola")
     }
 }
 
