284a53cc2be830840e50717f9fd8502ad009cee7
mgonzalez506 <MGonzalez506@users.noreply.github.com>
2023-01-23 17:26:53 -0600

    Create a class Book with two properties
    Create a class Book with two properties, BookTitle and BookAuthor

--- Playground-Challenges/L10-Challenge.playground/Contents.swift
+++ Playground-Challenges/L10-Challenge.playground/Contents.swift
@@ -9,3 +9,15 @@ class Person {
         name = fullName
     }
 }
+
+// Create a class Book with two properties
+// Book Title and Book Author
+class Book {
+    var title:String?
+    var author:String?
+    
+    init(_ bookTitle:String, _ bookAuthor:String) {
+        title = bookTitle
+        author = bookAuthor
+    }
+}
