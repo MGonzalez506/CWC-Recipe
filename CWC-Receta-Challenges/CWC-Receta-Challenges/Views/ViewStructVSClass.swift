ad55d4af098d85aba88391faa74a01aed39663b6
mgonzalez506 <MGonzalez506@users.noreply.github.com>
2023-01-23 15:53:04 -0600

    Move getRandomName to newNameModel
    Move getRandomName to newNameModel, in order to get a random new name from the class Names

--- CWC-Receta-Challenges/CWC-Receta-Challenges/Views/ViewStructVSClass.swift
+++ CWC-Receta-Challenges/CWC-Receta-Challenges/Views/ViewStructVSClass.swift
@@ -89,14 +89,6 @@ struct ViewStructVSClass: View {
         nameClass_2 = b.name
     }
     
-    // Function get random name from list of
-    // available names
-    func getRandomName() -> String {
-        // Generate random name from 1 to the amount of
-        // elements in the name array
-        let randNameNum = Int.random(in: 1...names.count)
-        return names[randNameNum-1]
-    }
 }
 
 struct ViewStructVSClass_Previews: PreviewProvider {
