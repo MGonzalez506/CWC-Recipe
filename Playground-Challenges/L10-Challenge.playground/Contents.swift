097c2f50cc721fd1e385094bc02ab41e1309b576
mgonzalez506 <MGonzalez506@users.noreply.github.com>
2023-01-24 22:08:40 -0600

    Add test unit
    Add test unit and make sure that the system is returning the expected behavior

--- Playground-Challenges/L10-Challenge.playground/Contents.swift
+++ Playground-Challenges/L10-Challenge.playground/Contents.swift
@@ -72,7 +72,7 @@ class Library {
             } else {
                 // Book has not being checked out
                 checkedOutBooks[bookId] = person
-                return "Succesfully checked out"
+                return "Successfully checked out"
             }
         } else {
             // Book does not exist in the catalogue
@@ -91,7 +91,7 @@ class Library {
                 if let _ = checkedOutBooks[key] {
                     // The book has checked out at some point
                     checkedOutBooks.removeValue(forKey: key)
-                    return "Succesfully checked in"
+                    return "Successfully checked in"
                 } else {
                     // The book has not been checked out
                     return "Error: Can't check in a book that hasn't been checked out"
@@ -106,4 +106,71 @@ class Library {
 }
 
 // Create instance of Library
-let libreria = Library()
+let lib = Library()
+let borrower1 = Person("Curious George")
+let borrower2 = Person("Mark Twain")
+
+print("1)")
+// George searches for a book
+// Expected Output in console:
+// "Available"
+print("Expected: Available")
+let searchResult = lib.searchByTitle("1984")
+print("Got:      \(searchResult) \n")
+
+
+print("2)")
+// George checks out the book
+// Expected Output in console:
+// "Successfully checked out"
+print("Expected: Successfully checked out")
+let borrowResult = lib.checkOut("ORW", borrower1)
+print("Got:      \(borrowResult) \n")
+
+
+print("3)")
+// Mark searches for a book
+// Expected Output in console:
+// "Checked out by Curious George"
+print("Expected: Checked out by Curious George")
+let searchResult2 = lib.searchByTitle("1984")
+print("Got:      \(searchResult2) \n")
+
+print("4)")
+// Mark tries to borrow a book that's already checked out
+// Expected Output in console:
+// "Error: Book already checked out"
+print("Expected: Error: Book already checked out")
+let borrowResult2 = lib.checkOut("ORW", borrower2)
+print("Got:      \(borrowResult2) \n")
+
+
+
+print("5)")
+// A book is checked in
+// Expected Output in console:
+// "Error: Can't check in a book that hasn't been checked out"
+print("Expected: Error: Can't check in a book that hasn't been checked out")
+let checkInResult = lib.checkIn("RAY")
+print("Got:      \(checkInResult) \n")
+
+
+print("6)")
+// George checks in his book
+// Expected Output in console:
+// "Successfully checked in"
+print("Expected: Successfully checked in")
+let checkInResult2 = lib.checkIn("ORW")
+print("Got:      \(checkInResult2) \n")
+
+
+print("7)")
+// Mark attempts to borrow the book again
+// Expected Output in console:
+// "Successfully checked out"
+print("Expected: Successfully checked out")
+let borrowResult3 = lib.checkOut("ORW", borrower2)
+print("Got:      \(borrowResult3) \n")
+
+
+
