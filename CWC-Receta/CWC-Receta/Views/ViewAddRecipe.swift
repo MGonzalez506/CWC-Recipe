182078336cf0eb7a13236066dd88274aed7ecd4c
mgonzalez506 <MGonzalez506@users.noreply.github.com>
2023-02-03 18:29:56 -0600

    Moving main View to separate View

---
+++ CWC-Receta/CWC-Receta/Views/ViewAddRecipe.swift
@@ -0,0 +1,20 @@
+//
+//  ViewAddRecipe.swift
+//  CWC-Receta
+//
+//  Created by MIGUEL GONZALEZ on 3/2/23.
+//
+
+import SwiftUI
+
+struct ViewAddRecipe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewAddRecipe_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewAddRecipe()
+    }
+}
