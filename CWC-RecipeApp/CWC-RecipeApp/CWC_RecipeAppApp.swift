2a934030264355274a724b3c365f1df01f7eda3c
mgonzalez506 <MGonzalez506@users.noreply.github.com>
2023-02-05 12:24:11 -0600

    Setup new project for L13

---
+++ CWC-RecipeApp/CWC-RecipeApp/CWC_RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWC_RecipeAppApp.swift
+//  CWC-RecipeApp
+//
+//  Created by MIGUEL GONZALEZ on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CWC_RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
