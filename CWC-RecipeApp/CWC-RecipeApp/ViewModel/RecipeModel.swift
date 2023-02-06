10d3b405bc609416c3a0181fc64331c2149d7831
mgonzalez506 <MGonzalez506@users.noreply.github.com>
2023-02-05 18:08:29 -0600

    Get the path to the json file
    Get the path to the json file within the app bundle

--- CWC-RecipeApp/CWC-RecipeApp/ViewModel/RecipeModel.swift
+++ CWC-RecipeApp/CWC-RecipeApp/ViewModel/RecipeModel.swift
@@ -13,6 +13,9 @@ class RecipeModel:ObservableObject {
     @Published var recipe = [Recipe]()
     
     init() {
+        // Get the path to the json file within the app bundle
+        let pathString = Bundle.main.path(forResource: "recipes", ofType: "json")
+        
         
     }
 }
