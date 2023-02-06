77522f53a8e9177417191d5df1e16ce2e341d81b
mgonzalez506 <MGonzalez506@users.noreply.github.com>
2023-02-06 02:59:10 -0600

    Embed in VStack

--- CWC-Receta-Challenges/CWC-Receta-Challenges/Views/ViewPizzasFromJSON.swift
+++ CWC-Receta-Challenges/CWC-Receta-Challenges/Views/ViewPizzasFromJSON.swift
@@ -14,26 +14,28 @@ struct ViewPizzasFromJSON: View {
     
     var body: some View {
         ScrollView {
-            ForEach(modelo.pizzas) { pizza in
-                HStack {
-                    Image(pizza.image)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 70, height: 50, alignment: .center)
-                        .clipped()
-                        .cornerRadius(5)
-                    VStack (alignment: .leading) {
-                        Text(pizza.name)
-                            .font(.title)
-                            .foregroundColor(.pink)
-                            .fontWeight(.bold)
-                        if let topping = pizza.toppings {
-                            HStack {
-                                ForEach (topping, id: \.self) { topp in
-                                    Text(topp)
-                                }
-                                .font(.subheadline)
+            VStack (alignment: .leading){
+                ForEach(modelo.pizzas) { pizza in
+                    HStack {
+                        Image(pizza.image)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 70, height: 50, alignment: .center)
+                            .clipped()
+                            .cornerRadius(5)
+                        VStack (alignment: .leading) {
+                            Text(pizza.name)
+                                .font(.title)
                                 .foregroundColor(.pink)
+                                .fontWeight(.bold)
+                            if let topping = pizza.toppings {
+                                HStack {
+                                    ForEach (topping, id: \.self) { topp in
+                                        Text(topp)
+                                    }
+                                    .font(.subheadline)
+                                    .foregroundColor(.pink)
+                                }
                             }
                         }
                     }
