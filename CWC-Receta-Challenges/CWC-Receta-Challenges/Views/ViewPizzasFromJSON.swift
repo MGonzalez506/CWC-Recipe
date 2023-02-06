d106adf8cbadcb7c289f1a61b4f908c45ba75aa3
mgonzalez506 <MGonzalez506@users.noreply.github.com>
2023-02-06 02:58:27 -0600

    Add data to the View

--- CWC-Receta-Challenges/CWC-Receta-Challenges/Views/ViewPizzasFromJSON.swift
+++ CWC-Receta-Challenges/CWC-Receta-Challenges/Views/ViewPizzasFromJSON.swift
@@ -15,7 +15,29 @@ struct ViewPizzasFromJSON: View {
     var body: some View {
         ScrollView {
             ForEach(modelo.pizzas) { pizza in
-                
+                HStack {
+                    Image(pizza.image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 70, height: 50, alignment: .center)
+                        .clipped()
+                        .cornerRadius(5)
+                    VStack (alignment: .leading) {
+                        Text(pizza.name)
+                            .font(.title)
+                            .foregroundColor(.pink)
+                            .fontWeight(.bold)
+                        if let topping = pizza.toppings {
+                            HStack {
+                                ForEach (topping, id: \.self) { topp in
+                                    Text(topp)
+                                }
+                                .font(.subheadline)
+                                .foregroundColor(.pink)
+                            }
+                        }
+                    }
+                }
             }
         }
     }
