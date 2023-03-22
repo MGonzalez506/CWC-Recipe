ca4a5f174d7a99a2548fc9482f37e3bc2d1b03d9
mgonzalez506 <MGonzalez506@users.noreply.github.com>
2023-03-21 20:37:32 -0600

    Add the variable that will hold all recipe information

---
+++ CWC-RecipeApp/CWC-RecipeApp/Views/RecipeDetailView.swift
@@ -0,0 +1,25 @@
+//
+//  RecipeDetailView.swift
+//  CWC-RecipeApp
+//
+//  Created by MIGUEL GONZALEZ on 21/3/23.
+//
+
+import SwiftUI
+
+struct RecipeDetailView: View {
+    
+    // Add the variable that will hold all information
+    // about a recipe
+    var recipe:Recipe
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeDetailView()
+    }
+}
