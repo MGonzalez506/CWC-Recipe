d1229a381a5c2a0e6cc0a6982e308a345e60b155
Miguel Gonzalez <MGonzalez506@users.noreply.github.com>
2024-01-07 15:32:20 -0600

    Add RecipeTabView.swift as the starting point for the app

---
+++ CWC-RecipeApp/CWC-RecipeApp/Views/RecipeTabView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeTabView.swift
+//  CWC-RecipeApp
+//
+//  Created by Miguel González on 2024-01-07.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeTabView()
+}
