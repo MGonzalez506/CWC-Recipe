1941470c6ed9e498422a1fce230e85abc44cb1da
Miguel Gonzalez <MGonzalez506@users.noreply.github.com>
2024-01-07 17:14:25 -0600

    Add tab views icons

---
+++ CWC-Receta-Challenges/CWC-Receta-Challenges/Views/ViewTabView.swift
@@ -0,0 +1,18 @@
+//
+//  ViewTabView.swift
+//  CWC-Receta-Challenges
+//
+//  Created by Miguel González on 2024-01-07.
+//
+
+import SwiftUI
+
+struct ViewTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewTabView()
+}
---
+++ CWC-Receta-Challenges/ViewTab1_Tortoise.swift
@@ -0,0 +1,18 @@
+//
+//  ViewTab1_Tortoise.swift
+//  CWC-Receta-Challenges
+//
+//  Created by Miguel González on 2024-01-07.
+//
+
+import SwiftUI
+
+struct ViewTab1_Tortoise: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewTab1_Tortoise()
+}
